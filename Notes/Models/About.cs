1f348221ea303fdb03640d64e3b2cedbb941e84a
Redjigame <ra.regeard@gmail.com>
2022-11-15 00:14:08 -0300

    note commit

---
+++ Notes/Models/About.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Models
+{
+    class About
+    {
+        public string Title => AppInfo.Name;
+        public string Version => AppInfo.VersionString;
+        public string MoreInfoUrl => "https://www.linkedin.com/in/renaud-regeard-620500233/";
+        public string Message => "This app is written in XAML and C# with .NET MAUI.";
+    }
+}
